0d0debf76c26b86c301ef3647d2b59d6490433f8
ismail <153764937+ismzrm@users.noreply.github.com>
2025-03-21 22:20:15 +0300

    record search
    record search

---
+++ 2025.03.18/2025.03.18/Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.button1 = New System.Windows.Forms.Button()
+        Me.textBox3 = New System.Windows.Forms.TextBox()
+        Me.comboBox1 = New System.Windows.Forms.ComboBox()
+        Me.textBox2 = New System.Windows.Forms.TextBox()
+        Me.textBox1 = New System.Windows.Forms.TextBox()
+        Me.dataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.dataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'button1
+        '
+        Me.button1.Location = New System.Drawing.Point(607, 393)
+        Me.button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.button1.Name = "button1"
+        Me.button1.Size = New System.Drawing.Size(251, 89)
+        Me.button1.TabIndex = 11
+        Me.button1.Text = "button1"
+        Me.button1.UseVisualStyleBackColor = True
+        '
+        'textBox3
+        '
+        Me.textBox3.Location = New System.Drawing.Point(344, 394)
+        Me.textBox3.Margin = New System.Windows.Forms.Padding(4)
+        Me.textBox3.Name = "textBox3"
+        Me.textBox3.Size = New System.Drawing.Size(253, 22)
+        Me.textBox3.TabIndex = 10
+        '
+        'comboBox1
+        '
+        Me.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.comboBox1.FormattingEnabled = True
+        Me.comboBox1.Items.AddRange(New Object() {"isime göre", "soyisime göre", "yaşa göre"})
+        Me.comboBox1.Location = New System.Drawing.Point(344, 361)
+        Me.comboBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.comboBox1.Name = "comboBox1"
+        Me.comboBox1.Size = New System.Drawing.Size(512, 24)
+        Me.comboBox1.TabIndex = 9
+        '
+        'textBox2
+        '
+        Me.textBox2.Location = New System.Drawing.Point(19, 393)
+        Me.textBox2.Margin = New System.Windows.Forms.Padding(4)
+        Me.textBox2.Name = "textBox2"
+        Me.textBox2.Size = New System.Drawing.Size(316, 22)
+        Me.textBox2.TabIndex = 8
+        '
+        'textBox1
+        '
+        Me.textBox1.Location = New System.Drawing.Point(19, 361)
+        Me.textBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.textBox1.Name = "textBox1"
+        Me.textBox1.Size = New System.Drawing.Size(316, 22)
+        Me.textBox1.TabIndex = 7
+        '
+        'dataGridView1
+        '
+        Me.dataGridView1.AllowUserToAddRows = False
+        Me.dataGridView1.AllowUserToDeleteRows = False
+        Me.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dataGridView1.Location = New System.Drawing.Point(3, 3)
+        Me.dataGridView1.Margin = New System.Windows.Forms.Padding(4)
+        Me.dataGridView1.Name = "dataGridView1"
+        Me.dataGridView1.ReadOnly = True
+        Me.dataGridView1.Size = New System.Drawing.Size(1045, 351)
+        Me.dataGridView1.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(1051, 529)
+        Me.Controls.Add(Me.button1)
+        Me.Controls.Add(Me.textBox3)
+        Me.Controls.Add(Me.comboBox1)
+        Me.Controls.Add(Me.textBox2)
+        Me.Controls.Add(Me.textBox1)
+        Me.Controls.Add(Me.dataGridView1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.dataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Private WithEvents button1 As System.Windows.Forms.Button
+    Private WithEvents textBox3 As System.Windows.Forms.TextBox
+    Private WithEvents comboBox1 As System.Windows.Forms.ComboBox
+    Private WithEvents textBox2 As System.Windows.Forms.TextBox
+    Private WithEvents textBox1 As System.Windows.Forms.TextBox
+    Private WithEvents dataGridView1 As System.Windows.Forms.DataGridView
+
+End Class
---
+++ 2025.03.18/2025.03.18/Form1.vb
@@ -0,0 +1,73 @@
+﻿Imports System.Data.OleDb
+
+Public Class Form1
+    Dim baglan As OleDbConnection = New OleDbConnection _
+    ("Provider=Microsoft.Jet.Oledb.4.0;Data Source=bilgiler1.mdb")
+    Sub gnc()
+        Dim tablo As DataTable = New DataTable()
+        Dim adaptor As OleDbDataAdapter = New OleDbDataAdapter _
+        ("SELECT * FROM TABLO1", baglan)
+        adaptor.Fill(tablo)
+        dataGridView1.DataSource = tablo
+
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        gnc()
+
+    End Sub
+
+    Private Sub textBox1_TextChanged(sender As Object, e As EventArgs) Handles textBox1.TextChanged
+        Dim tablo As DataTable = New DataTable()
+        Dim adaptor As OleDbDataAdapter = New OleDbDataAdapter _
+        ("SELECT * FROM TABLO1 WHERE ADI+SOYADI+MEMLEKET LIKE '%" + textBox1.Text + "%'", baglan)
+        adaptor.Fill(tablo)
+        dataGridView1.DataSource = tablo
+
+    End Sub
+
+    Private Sub textBox2_TextChanged(sender As Object, e As EventArgs) Handles textBox2.TextChanged
+        Dim tablo As New DataTable()
+        Dim adaptor As New OleDbDataAdapter _
+            ("SELECT * FROM TABLO1 WHERE ADI+SOYADI+MEMLEKET LIKE '%" + textBox2.Text + "%'", baglan)
+        adaptor.Fill(tablo)
+        dataGridView1.DataSource = tablo
+    End Sub
+
+    Private Sub button1_Click(sender As Object, e As EventArgs) Handles button1.Click
+        If (comboBox1.SelectedIndex = -1) Then
+            MsgBox("Lütfen seviye seçiniz")
+        End If
+        If (textBox3.Text.Trim() = "") Then
+            MsgBox("Lütfen boş bırakmayınız")
+        End If
+        Dim sql1 As String = "SELECT * FROM TABLO1 WHERE ADI=?  "
+        Dim sql2 As String = "SELECT * FROM TABLO1 WHERE SOYADI=?"
+        Dim sql3 As String = "SELECT * FROM TABLO1 WHERE YAS=?"
+
+        Dim ara As OleDbCommand = New OleDbCommand()
+        ara.Connection = baglan
+        ara.Parameters.AddWithValue("?", textBox3.Text)
+        Select Case comboBox1.SelectedIndex
+            Case 0
+                ara.CommandText = sql1
+            Case 1
+                ara.CommandText = sql2
+            Case 2
+                ara.CommandText = sql3
+        End Select
+        Dim adaptor As OleDbDataAdapter = New OleDbDataAdapter(ara)
+        Dim tablo As DataTable = New DataTable()
+        adaptor.Fill(tablo)
+        If (tablo.Rows.Count = 0) Then
+            MessageBox.Show("ARANAN KAYIT SİSTEMDE YOK")
+            gnc()
+        Else
+            dataGridView1.DataSource = tablo
+            textBox1.Text = tablo.Rows(0)("ADI").ToString()
+            textBox2.Text = tablo.Rows(0)("SOYADI").ToString()
+        End If
+
+
+
+    End Sub
+End Class
